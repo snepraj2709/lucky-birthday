fcf96a4859f51eeafe8f01207afc97f730a7c8db
snepraj2709 <snehaprajapatijnv15@gmail.com>
2023-02-04 15:38:17 +0530

    removed error-added styling

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const userBirthday = document.querySelector("#user-birthday");
 const luckyNumber = document.querySelector("#user-lucky-number");
-const checkLuckyBirthday = document.querySelector("#check-lucky-birthday");
+const checkLuckyBirthday = document.querySelector("#check-button");
 const outputMessage = document.querySelector(".output-message");
 
 // Now, find out if sum of digits of birthday is divisible by the lucky number.
 
 checkLuckyBirthday.addEventListener("click", function isBirthdayLucky() {
-  console.log(typeof userBirthday.value);
-  console.log(typeof luckyNumber.value);
-  console.log("Clicked Button");
-
   const birthDay = userBirthday.value;
-  const sumTotal = calculateSum(birthDay);
   const luckyNo = luckyNumber.value;
 
-  if (sumTotal % luckyNo === 0) {
-    outputMessage("Your birthday is lucky");
+  if (birthDay & luckyNo) {
+    const sumTotal = calculateSum(birthDay);
+    if (sumTotal % luckyNo === 0) {
+      printMessage("Your birthday is lucky 💃");
+    } else {
+      printMessage("Your birthday is not lucky 👻");
+    }
   } else {
-    outputMessage("Your birthday is not lucky");
+    printMessage("Please enter both the numbers 🙄");
   }
 });
 
@@ -25,14 +25,13 @@ function calculateSum(birthDay) {
   let sum = 0;
   birthDay = birthDay.replaceAll("-", "");
   for (i = 0; i < birthDay.length; i++) {
-    sum = sum + Number(birthDay.charAt[i]);
-    console.log(i);
-    console.log(sum);
+    sum = sum + Number(birthDay.charAt(i));
   }
+  console.log(sum);
   return sum;
 }
 
-function outputMessage(mssg) {
+function printMessage(mssg) {
   outputMessage.style.display = "block";
   outputMessage.innerText = mssg;
 }
